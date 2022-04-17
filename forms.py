09c16a1edfe4f846a401ed328bf918f992a29ba1
Bett-C <collins.kkiprotich.student@moringaschool.com>
2022-04-18 00:51:11 +0300

    it will register in class admin

--- forms.py
+++ forms.py
@@ -4,15 +4,18 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Business, NeighbourHood
 
+
 class CreateUserForm(UserCreationForm):
     class Meta:
         model=User
         fields=['username','email','password1','password2']
+
 class BusinessForm(forms.ModelForm):
     class Meta:
         model=Business
         fields=['name','email','user','neighbourhood']
-        class NeighbourhoodForm(forms.ModelForm):
-class Meta:
+
+class NeighbourhoodForm(forms.ModelForm):
+    class Meta:
         model=NeighbourHood
         fields=['name','location','occupants_count']
