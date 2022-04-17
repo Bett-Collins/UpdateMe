83c34a21b55589f1340003a662752c334df439ad
Bett-C <collins.kkiprotich.student@moringaschool.com>
2022-04-17 23:30:18 +0300

    adding class neighborhood

--- models.py
+++ models.py
@@ -16,4 +16,21 @@ class Admin(models.Model):
     def delete_admin(self):
         self.delete()
 
-# Create your models here.
+
+class NeighbourHood(models.Model):
+    name=models.CharField(max_length=50,null=False)
+    location=models.CharField(max_length=50,null=False)
+    occupants_count=models.IntegerField(default=0,null=False)
+    admin=models.ForeignKey(Admin, on_delete=CASCADE,null=True)
+
+    def __str__(self):
+        return self.name
+
+    def create_neighbourhood(self):
+       pass
+
+    def delete_neighbourhood(self):
+        self.delete()
+
+    def update_neighbourhood(self):
+        pass
