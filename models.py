24b315a52c5f43fc4bd25d3da5c31da302e96a55
Bett-C <collins.kkiprotich.student@moringaschool.com>
2022-04-17 23:29:15 +0300

    adding class in model

--- models.py
+++ models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.db import models
+
+
+# Create your models here.
+
+class Admin(models.Model):
+    name=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    def save_admin(self):
+        self.save()
+
+    def delete_admin(self):
+        self.delete()
 
 # Create your models here.
