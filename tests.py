e49f820979063a63b52270a63c1e242007c3b9d6
Bett-C <collins.kkiprotich.student@moringaschool.com>
2022-04-18 00:56:43 +0300

    creating test for business

--- tests.py
+++ tests.py
@@ -5,3 +5,11 @@ class BusinessTestClass(TestCase):
     #setup method
     def setUp(self):
         self.business=Business(name='cereals',email="a@gmail.com")
+ #Testing Instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.business,Business))
+
+    def test_create_business(self):
+        self.business.create_business()
+        bus1=Business.objects.all()
+        self.assertIsNotNone(bus1)
