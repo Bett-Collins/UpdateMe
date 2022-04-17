057d14fec11303236ffd18a651570862efd1ab00
Bett-C <collins.kkiprotich.student@moringaschool.com>
2022-04-17 23:33:38 +0300

    it will add index function in views

--- views.py
+++ views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    businesses=Business.objects.all()
+
+    if 'item_name' in request.GET:
+        item_name=request.GET['item_name']
+        businesses=businesses.filter(name__icontains=item_name)
+    else:
+        businesses=Business.objects.all()
+
+    users=User.objects.all()
+    neighbourhoods=NeighbourHood.objects.all()
+
+
+   
