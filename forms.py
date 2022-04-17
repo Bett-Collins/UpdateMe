f724d07e46340d623ed615201d50eda74dad422f
Bett-C <collins.kkiprotich.student@moringaschool.com>
2022-04-18 00:47:43 +0300

    add BussinessForm

--- forms.py
+++ forms.py
@@ -8,3 +8,7 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model=User
         fields=['username','email','password1','password2']
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model=Business
+        fields=['name','email','user','neighbourhood']
