5751eb784e92090bae5d800b50c4e451ff98e1ec
Bett-C <collins.kkiprotich.student@moringaschool.com>
2022-04-17 23:43:21 +0300

    adding login function

--- views.py
+++ views.py
@@ -33,6 +33,25 @@ def registeruser(request):
                         }
 
     return render(request,'registration/register.html',context)
+def loginpage(request):
+    if request.user.is_authenticated:
+
+            return redirect('index')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password =request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+            else:
+                messages.info(request, 'Username or password is incorrect')
+
+      
+    return render(request,'registration/login.html')
 
 
    
