70f850ea9af404af6b32a1d6cc7c31d5b164ce86
Bett-C <collins.kkiprotich.student@moringaschool.com>
2022-04-18 00:52:33 +0300

    adding test

--- tests.py
+++ tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+class BusinessTestClass(TestCase):
+    #setup method
+    def setUp(self):
+        self.business=Business(name='cereals',email="a@gmail.com")
